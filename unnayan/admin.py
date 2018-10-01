40459feb57ba923f6dc0cb40ae00dec4c8eb20b0
Nagendra Srivastava <sanu.nagendra@gmail.com>
2018-10-02 01:29:21 +0530

    company profile added to admin

--- unnayan/admin.py
+++ unnayan/admin.py
@@ -2,7 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from models import Client, Application, ApplicationConfig, AppUserInfo,AppVersions
+from models import Client, Application, ApplicationConfig, AppUserInfo, AppVersions, CompanyProfile
+
 
 # Register your models here.
 # 
@@ -28,8 +29,13 @@ class AppVersionsAdmin(admin.ModelAdmin):
     pass
 
 
+class CompanyProfileAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Client, ClientAdmin)
 admin.site.register(Application, ApplicationAdmin)
 admin.site.register(ApplicationConfig, ApplicationConfigAdmin)
 admin.site.register(AppUserInfo, AppUserInfoAdmin)
 admin.site.register(AppVersions, AppVersionsAdmin)
+admin.site.register(CompanyProfile, CompanyProfileAdmin)
