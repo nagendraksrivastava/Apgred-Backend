61a49a5c1ecc9ad608478fd5b1c2e1da5f4252f5
Nagendra Srivastava <sanu.nagendra@gmail.com>
2018-10-02 23:43:44 +0530

    CORS header support added

--- apgred/settings.py
+++ apgred/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'dashboard',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'apgred.urls'
 
