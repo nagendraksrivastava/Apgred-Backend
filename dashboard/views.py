ffebe48028f9b286a9b437822452184fc59bbeaa
Nagendra Srivastava <sanu.nagendra@gmail.com>
2018-10-26 18:03:11 +0530

    app based filter for users

--- dashboard/views.py
+++ dashboard/views.py
@@ -22,13 +22,22 @@ def total_user(request):
     if request.method != 'GET':
         raise MethodNotAllowed
     token_value = get_authorization_header(request)
+    app_token = request.GET['app_token']
+
     try:
         token = Token.objects.get(key=token_value)
-        total_user_count = AppUserInfo.objects.all().count()
-        data = {'total_user': total_user_count}
-        return HttpResponse(json.dumps(data))
     except Token.DoesNotExist:
         raise AuthenticationFailed
+
+    try:
+        application = Application.objects.get(app_token=app_token)
+    except Application.DoesNotExist:
+        json_result = {"status": {"code": 301, "message": "Client registered but app not registered "}}
+        return HttpResponse(json.dumps(json_result))
+
+    total_user_count = AppUserInfo.objects.all().filter(app=application).count()
+    data = {'total_user': total_user_count}
+    return HttpResponse(json.dumps(data))
 
 
 def get_active_user_count(request):
@@ -36,30 +45,39 @@ def get_active_user_count(request):
         raise MethodNotAllowed
     token_value = get_authorization_header(request)
     filter_param = request.GET['active']
+    app_token = request.GET['app_token']
+
     try:
         token = Token.objects.get(key=token_value)
-        if filter_param == 'daily':
-            # daily_active_user_count = AppUserInfo.objects.filter(Q(modified=date.today()) | Q(created=date.today()))
-            data = {"daily_active_user": get_daily_active_user()}
-            return HttpResponse(json.dumps(data))
-        if filter_param == 'weekly':
-            data = {"weekly_active_user": get_weekly_active_user()}
-            return HttpResponse(json.dumps(data))
-
-        if filter_param == 'monthly':
-            data = {"monthly_active_user": get_monthly_active_user()}
-            return HttpResponse(json.dumps(data))
-        if filter_param == "all":
-            data = {
-
-                "daily_active_user": get_daily_active_user(),
-                "weekly_active_user": get_weekly_active_user(),
-                "monthly_active_user": get_monthly_active_user()
-
-            }
-            return HttpResponse(json.dumps(data))
     except Token.DoesNotExist:
         raise AuthenticationFailed
+
+    try:
+        application = Application.objects.get(app_token=app_token)
+    except Application.DoesNotExist:
+        json_result = {"status": {"code": 301, "message": "Client registered but app not registered "}}
+        return HttpResponse(json.dumps(json_result))
+
+    if filter_param == 'daily':
+        # daily_active_user_count = AppUserInfo.objects.filter(Q(modified=date.today()) | Q(created=date.today()))
+        data = {"daily_active_user": get_daily_active_user(application)}
+        return HttpResponse(json.dumps(data))
+    if filter_param == 'weekly':
+        data = {"weekly_active_user": get_weekly_active_user(application)}
+        return HttpResponse(json.dumps(data))
+
+    if filter_param == 'monthly':
+        data = {"monthly_active_user": get_monthly_active_user(application)}
+        return HttpResponse(json.dumps(data))
+    if filter_param == "all":
+        data = {
+
+            "daily_active_user": get_daily_active_user(application),
+            "weekly_active_user": get_weekly_active_user(application),
+            "monthly_active_user": get_monthly_active_user(application)
+
+        }
+        return HttpResponse(json.dumps(data))
 
 
 def last_time_update_triggered(request):
@@ -181,18 +199,22 @@ def get_app_users(request):
             user_data = AppUserInfo.objects.all
 
 
-def get_daily_active_user():
-    return AppUserInfo.objects.filter(modified=date.today()).count()
+def get_daily_active_user(application):
+    return AppUserInfo.objects.filter(modified=date.today(),
+                                      app=application).count()
 
 
-def get_weekly_active_user():
+def get_weekly_active_user(application):
     some_day_last_week = timezone.now().date() - timedelta(days=7)
     monday_of_last_week = some_day_last_week - timedelta(days=(some_day_last_week.isocalendar()[2] - 1))
     monday_of_this_week = monday_of_last_week + timedelta(days=7)
     return AppUserInfo.objects.filter(modified__gte=monday_of_last_week,
-                                      modified__lt=monday_of_this_week).count()
+                                      modified__lt=monday_of_this_week,
+                                      app=application
+                                      ).count()
 
 
-def get_monthly_active_user():
+def get_monthly_active_user(application):
     start_of_month = date.today().replace(day=1)
-    return AppUserInfo.objects.filter(modified__gte=start_of_month).count()
+    return AppUserInfo.objects.filter(modified__gte=start_of_month,
+                                      app=application).count()
